53aea595abe9c09a4a47bc1d207e4ddc0fb8e02e
Hamdi Nour <nourhamdi266@gmail.com>
2026-08-18 19:02:50 +0100

    Add files via upload

---
+++ orchestration/config.py
@@ -0,0 +1,9 @@
+import os
+
+# Database Connection Configuration
+DB_CONFIG = {
+    'server': r'localhost\SQLEXPRESS',
+    'database': 'DataWarehouse',
+    'driver': 'ODBC Driver 17 for SQL Server',
+    'trusted_connection': 'yes'
+}
