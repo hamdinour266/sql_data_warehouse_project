2af2d4f0f473f07373f070578e96233599595722
Hamdi Nour <nourhamdi266@gmail.com>
2026-08-18 19:05:16 +0100

    Delete orchestration directory

--- orchestration/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Database Connection Configuration
-DB_CONFIG = {
-    'server': r'localhost\SQLEXPRESS',
-    'database': 'DataWarehouse',
-    'driver': 'ODBC Driver 17 for SQL Server',
-    'trusted_connection': 'yes'
-}
